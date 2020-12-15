9ad8c15609f3424cb9cd8bcfb524acb95986d0de
MarinkoBa <64740488+MarinkoBa@users.noreply.github.com>
2020-12-15 21:28:51 +0100

    add tweet availability check

--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
-import json
 import os
-
+from decouple import config
 from src.utils.get_tweets_by_id import get_tweets_by_id
 
 
-
-if __name__ == "__main__":  
-    # load configuration fields
-    config = json.load(open('./config.json', 'r'))
-    
+if __name__ == "__main__":
 
     df = get_tweets_by_id(config,
                           os.path.join('data', 'NAACL_SRW_2016.csv'))
--- src/utils/get_tweets_by_id.py
+++ src/utils/get_tweets_by_id.py
@@ -1,10 +1,11 @@
 import tweepy
 import pandas as pd
 
+
 def load_data(file_path):
-    '''
+    """
     Load a csv data set in a pandas dataframe.
-    
+
     Parameters
     ----------
     file_path:      String
@@ -13,21 +14,37 @@ def load_data(file_path):
     Returns
     -------
     df:             Pandas dataframe
-                    The dataframe containing the data from the csv file.              
-    '''
+                    The dataframe containing the data from the csv file.
+    """
     df = pd.read_csv(file_path,
-                     sep = ',',
-                     header = None,
-                     names = ['tweet_id', 'label'])
+                     sep=',',
+                     header=None,
+                     names=['tweet_id', 'label'])
     return df
 
 
+def export_data(df, file_path):
+    """
+    Export a pandas dataframe including tweet text as a csv file.
+
+    Parameters
+    ----------
+    df:             DataFrame
+                    The DataFrame object including tweet id, tweet label and tweet text.
+    file_path:      String
+                    The path to the output csv file.
+    """
+    df.to_csv(file_path,
+              sep=',',
+              header=None,
+              index=False)
+
 
 def get_tweets_by_id(config, file_path):
-    '''
+    """
     Get the text from all available tweets with IDs in annotated dataset and
     save it in the dataframe.
-    
+
     Parameters
     ----------
     config:         JSON-object
@@ -40,21 +57,26 @@ def get_tweets_by_id(config, file_path):
     -------
     df:             Pandas dataframe
                     The dataframe containing the data from the csv file and the
-                    texts of the tweets if available.              
-    '''
+                    texts of the tweets if available.
+    """
     # load authorization details from config-file and send request to api
-    auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], config['CONSUMER_SECRET'])
-    auth.set_access_token(config['OAUTH_TOKEN'], config['OAUTH_TOKEN_SECRET'])
+    auth = tweepy.OAuthHandler(config('CONSUMER_KEY'), config('CONSUMER_SECRET'))
+    auth.set_access_token(config('OAUTH_TOKEN'), config('OAUTH_TOKEN_SECRET'))
     api = tweepy.API(auth)
-    
+
     # load annotated data set with read_data()
     df = load_data(file_path)
-    
-    ### TRY OUT, else via loop -> api.get_status(tweet_id).text
-    ### FIXME, what happens if tweet is no longer available? -> count?
-    df['text'] = df.tweet_id.apply(api.get_status).text
-    
+
+    df['text'] = df.tweet_id.apply(lambda x: get_tweet_text(x, api))
+
     ### TODO: can get information about geolocation (if available),
     #   have a look at examples when authorization works
 
     return df
+
+
+def get_tweet_text(tweet_id, api):
+    try:
+        return api.get_status(tweet_id).text  # return the text from the tweet with ID if tweet available
+    except:
+        return None                           # return None
