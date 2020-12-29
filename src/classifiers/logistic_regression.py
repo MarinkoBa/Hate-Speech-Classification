60f199751563df8d88b0bd111341d05fd37e7f04
danitratz <dratzweinmann@gmx.de>
2020-12-29 11:22:06 +0100

    basic training of logistic regression possible

--- src/classifiers/logistic_regression.py
+++ src/classifiers/logistic_regression.py
@@ -12,7 +12,7 @@ def define_features_vectorizer(df, columns, training_data,testing_data):
     ----------
     df:             	Pandas dataframe
                     	The dataframe containing both training_data and testing_data
-    column:         	String
+    column:         	String or list of strings if using multiple columns
                     	Names of columns of df that are used for trainig the classifier
     training_data:  	Pandas dataframe  
                     	The dataframe containing the training data for the classifier
@@ -46,13 +46,13 @@ def define_features_vectorizer(df, columns, training_data,testing_data):
     
 def define_features_tfidf(df, columns, training_data,testing_data):
     """
-    Define the features fro classification using CountVectorizer.
+    Define the features fro classification using TFIDF.
 
     Parameters
     ----------
     df:             	Pandas dataframe
                     	The dataframe containing both training_data and testing_data
-    column:         	String
+    column:         	String or list of strings if using multiple columns
                     	Names of columns of df that are used for trainig the classifier
     training_data:  	Pandas dataframe  
                     	The dataframe containing the training data for the classifier
@@ -80,6 +80,62 @@ def define_features_tfidf(df, columns, training_data,testing_data):
     
     
     return vectorizer, training_features, testing_features
+    
+    
+def setup_log_reg_classifier(df, training_data, testing_data, features, method="count"):
+    """
+    Define the features fro classification using TFIDF.
+
+    Parameters
+    ----------
+    df:             	Pandas dataframe
+                    	The dataframe containing both training_data and testing_data
+    training_data:  	Pandas dataframe  
+                    	The dataframe containing the training data for the classifier
+    testing_data:   	Pandas dataframe  
+                    	The dataframe containing the testing data for the classifier
+    features:         	String or list of strings if using multiple features
+                    	Names of columns of df that are used for trainig the classifier
+    method: 		String
+    			Can be either "count" or "tfidf" for specifying method of feature weighting
+                    
+
+    Returns
+    -------
+    vectorizer:     	sklearn CountVectorizer
+                    	CountVectorizer fit and transformed for training data
+    training_features: sparse matrix
+    			Document-term matrix for training data
+    testing_features:  sparse matrix
+    			Document-term matrix for testing data
+    """
+    
+
+    #generate x and y training data
+    
+    y_training=training_data["hate_speech"].values
+    
+    if method=="count":
+        vec, x_training, x_testing = define_features_vectorizer(df, features, training_data, testing_data)
+    elif method=="tfidf":
+        vec, x_training, x_testing = define_features_vectorizer(df, features, training_data, testing_data)
+    else:
+        print("Method has to be either count or tfidf")
+        return 1
+    
+    #train classifier
+    
+    log_reg_classifier=LogisticRegression(max_iter=1000)
+    model=log_reg_classifier.fit(x_training,y_training)
+    
+    return model
+    
+    
+
+    	
+	
+
+	
     
     
  
