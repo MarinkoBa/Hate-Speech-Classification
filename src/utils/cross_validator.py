351cd19bf760b510f335c3dc552401bdb31d300c
MarinkoBa <64740488+MarinkoBa@users.noreply.github.com>
2021-01-31 17:35:46 +0100

    added method for calculating the metrics in cross validation

--- src/utils/cross_validator.py
+++ src/utils/cross_validator.py
@@ -17,7 +17,7 @@ def cross_validate(x, y, n_splits=10):
               The dataframe containing all x values -> preprocessed, shape (n, 1).
     y:		  Pandas dataframe
               The dataframe containing all y values -> hate_speech, shape (n, 1).
-    n_splits:		 Integer
+    n_splits: Integer
               Number of folds
 
     """
@@ -42,7 +42,6 @@ def cross_validate(x, y, n_splits=10):
     tot_rec_ran_for = []
     tot_rec_log_reg = []
 
-
     kf = KFold(n_splits)
     for i, (train, test) in enumerate(kf.split(x)):
         x_train = x.iloc[train]
@@ -55,10 +54,7 @@ def cross_validate(x, y, n_splits=10):
         print('SVM: ')
         model_svm, vec_svm, X_testing_svm  = svm_classifier.setup_svm_classifier(x_train, y_train, x_test)
         y_pred_svm = svm_classifier.predict(model_svm, X_testing_svm)
-        error_svm = np.count_nonzero(y_pred_svm != np.array(y_test).flatten()) / float(len(test))
-        acc_svm = metrics.accuracy_score(y_test, y_pred_svm)
-        prec_svm = metrics.precision_score(y_test, y_pred_svm)
-        rec_svm = metrics.recall_score(y_test, y_pred_svm)
+        error_svm, acc_svm, prec_svm, rec_svm = calculate_metrics(y_test, y_pred_svm)
         print('Error: ' + str(error_svm))
         print('Accuracy: ' + str(acc_svm))
         print('Precision: ' + str(prec_svm))
@@ -68,10 +64,7 @@ def cross_validate(x, y, n_splits=10):
         print('Decision Tree: ')
         model_dec_tree, vec_dec_tree, X_testing_dec_tree = decision_tree_classifier.setup_decision_tree_classifier(x_train, y_train, x_test)
         y_pred_dec_tree = decision_tree_classifier.predict(model_dec_tree, X_testing_dec_tree)
-        error_dec_tree = np.count_nonzero(y_pred_dec_tree != np.array(y_test).flatten()) / float(len(test))
-        acc_dec_tree = metrics.accuracy_score(y_test, y_pred_dec_tree)
-        prec_dec_tree = metrics.precision_score(y_test, y_pred_dec_tree)
-        rec_dec_tree = metrics.recall_score(y_test, y_pred_dec_tree)
+        error_dec_tree, acc_dec_tree, prec_dec_tree, rec_dec_tree = calculate_metrics(y_test, y_pred_dec_tree)
         print('Error: ' + str(error_dec_tree))
         print('Accuracy: ' + str(acc_dec_tree))
         print('Precision: ' + str(prec_dec_tree))
@@ -81,10 +74,7 @@ def cross_validate(x, y, n_splits=10):
         print('Random Forrest: ')
         model_ran_for, vec_ran_for, X_testing_ran_for = random_forest_classifier.setup_random_forest_classifier(x_train, y_train, x_test)
         y_pred_ran_for = random_forest_classifier.predict(model_ran_for, X_testing_ran_for)
-        error_ran_for = np.count_nonzero(y_pred_ran_for != np.array(y_test).flatten()) / float(len(test))
-        acc_ran_for = metrics.accuracy_score(y_test, y_pred_ran_for)
-        prec_ran_for = metrics.precision_score(y_test, y_pred_ran_for)
-        rec_ran_for = metrics.recall_score(y_test, y_pred_ran_for)
+        error_ran_for, acc_ran_for, prec_ran_for, rec_ran_for = calculate_metrics(y_test, y_pred_ran_for)
         print('Error: ' + str(error_ran_for))
         print('Accuracy: ' + str(acc_ran_for))
         print('Precision: ' + str(prec_ran_for))
@@ -94,10 +84,7 @@ def cross_validate(x, y, n_splits=10):
         print('Logistic Regression: ')
         model_log_reg, vec_log_reg, X_testing_log_reg = logistic_regression.setup_log_reg_classifier(x_train, y_train, x_test)
         y_pred_log_reg = logistic_regression.predict(model_log_reg, X_testing_log_reg)
-        error_log_reg = np.count_nonzero(y_pred_log_reg != np.array(y_test).flatten()) / float(len(test))
-        acc_log_reg = metrics.accuracy_score(y_test, y_pred_log_reg)
-        prec_log_reg = metrics.precision_score(y_test, y_pred_log_reg)
-        rec_log_reg = metrics.recall_score(y_test, y_pred_log_reg)
+        error_log_reg, acc_log_reg, prec_log_reg, rec_log_reg = calculate_metrics(y_test, y_pred_log_reg)
         print('Error: ' + str(error_log_reg))
         print('Accuracy: ' + str(acc_log_reg))
         print('Precision: ' + str(prec_log_reg))
@@ -149,6 +136,39 @@ def cross_validate(x, y, n_splits=10):
     print('Avg recall Decision Tree: ' + str(np.average(tot_rec_dec_tree)))
     print('Avg recall Random Forrest: ' + str(np.average(tot_rec_ran_for)))
     print('Avg recall Logistic Regression: ' + str(np.average(tot_rec_log_reg)))
+
+
+def calculate_metrics(y_test, y_pred):
+    """
+    Calculate metrics: error, accuracy, precision & recall.
+
+    Parameters
+    ----------
+    y_test:        Pandas dataframe
+                   The dataframe containing ground truth test data.
+    y_pred:		   Pandas dataframe
+                   The dataframe containing prediction to the test data.
+
+    Returns
+    -------
+    error:		   float
+    			   proportion of false predictions w.r.t all data
+    accuracy:	   float
+    			   proportion of correct predictions w.r.t all data
+    precision:	   float
+    			   proportion of correct predictions w.r.t true positives and false positives
+    recall:		   float
+    			   proportion of correct predictions w.r.t true positives and false negatives
+
+    """
+    error = np.count_nonzero(y_pred != np.array(y_test).flatten()) / float(len(y_test))
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    precision = metrics.precision_score(y_test, y_pred)
+    recall = metrics.recall_score(y_test, y_pred)
+
+    return error, accuracy, precision, recall
+
+
 
 
 
