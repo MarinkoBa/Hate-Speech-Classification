4ba9ef7c7cab96428baf30720240695d044816b4
MarinkoBa <64740488+MarinkoBa@users.noreply.github.com>
2021-02-15 12:11:44 +0100

    added svm model and vec (Option 4)

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from src.utils.get_data import concatenate_datasets
 from src.utils.get_data import split_data
 from src.utils.preprocessing import preprocessing
 from src.utils.preprocessing import preprocessing_restricted
-from src.classifiers.ensemble_classifier import EnsembleClassifier
+from src.classifiers import svm_classifier
 from src.utils import cross_validator
 from src.utils import usa_hate_speech_calculator
 from src.utils import manage_classifiers
@@ -53,18 +53,17 @@ if __name__ == "__main__":
     df_usa['preprocessed'] = df_usa['full_text'].apply(preprocessing_restricted)
 
     # Save Model and Vectorizer
-    #ensemble = EnsembleClassifier()
-    #x_test, vec = ensemble.train(x_balanced, y_balanced, df_usa[['preprocessed']], method="tfidf", ngrams=(1, 1))
-    #y_pred_ens = ensemble.predict(x_test)
-    #manage_classifiers.save_classifier(ensemble, vec)
+    # svm_model, vec, x_test = svm_classifier.setup_svm_classifier(x_balanced, y_balanced, df_usa[['preprocessed']], method="tfidf",ngrams=(1,2))
+    # y_pred_svm = svm_classifier.predict(svm_model, x_test)
+    # manage_classifiers.save_classifier(svm_model, vec)
 
     # Load classifier
-    ensemble, vec = manage_classifiers.load_classifier(r'C:\Users\marin\Desktop\Hate-Speech-Classification\src\models\model.pkl', r'C:\Users\marin\Desktop\Hate-Speech-Classification\src\models\vec.pkl')
+    svm_model, vec = manage_classifiers.load_classifier(os.path.join(os.path.pardir, 'src', 'models', 'model.pkl'), os.path.join(os.path.pardir, 'src', 'models', 'vec.pkl'))
     x_test = vec.transform(df_usa['preprocessed'].values)
-    y_pred_ens = ensemble.predict(x_test)
+    y_pred_svm = svm_model.predict(x_test)
 
     # Calculate avg hate speech per US state
-    avg_hate_speech_per_state = usa_hate_speech_calculator.calculate_hate_speech_ratio(df_usa, y_pred_ens)
+    avg_hate_speech_per_state = usa_hate_speech_calculator.calculate_hate_speech_ratio(df_usa, y_pred_svm)
 
     unique_city_names = df_usa.city_name.unique()
 
