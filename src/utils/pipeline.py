bbac792160376fee96cb13a95810397193933799
Fabio Gebhard <fabiogebhard@gmail.com>
2021-02-23 19:46:00 +0100

    create run_experiment() in pipeline.py to evaluate parameters and classifier

--- src/utils/pipeline.py
+++ src/utils/pipeline.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-import constant
+from src.utils import constant
 from decouple import config
 from src.utils.get_data import get_tweets_by_id
 from src.utils.get_data import load_data
@@ -11,16 +11,16 @@ from src.utils.get_data import split_data
 from src.utils.get_data import get_datasets
 from src.utils.preprocessing import preprocessing
 from src.utils.preprocessing import preprocessing_restricted
-from src.classifiers import svm_classifier
-from src.utils import cross_validator
+from src.utils.get_data import load_labeled_dataset
+from src.utils.cross_validator import validate_parameters_via_cross_validation
 from src.utils import usa_hate_speech_calculator
 from src.utils import manage_classifiers
 from src.utils import dataset_balancer
 
 
-def run_pipeline(preprocessing = 'preprocessing_restricted'):
+def run_experiment(preprocessing = 'preprocessing_restricted'):
     # loads and concatenates the different datasets
-    df_dataset = load_data()
+    df_dataset = load_labeled_dataset()
 
     # runs chosen preprocess-method on the text-column of the dataframe
     if preprocessing is constant.PREPROCESSING_RESTRICTED:
@@ -32,5 +32,9 @@ def run_pipeline(preprocessing = 'preprocessing_restricted'):
     # balance data -> ~9k normal vs ~9k hate speech tweets
     x_balanced, y_balanced = dataset_balancer.balance_data(df_dataset[['preprocessed']], df_dataset[['hate_speech']])
 
+    # cross validation TODO change back [:1000]
+    parameters = validate_parameters_via_cross_validation(x_data=x_balanced[:1000],y_data=y_balanced[:1000])
+
+    return parameters
 
 
